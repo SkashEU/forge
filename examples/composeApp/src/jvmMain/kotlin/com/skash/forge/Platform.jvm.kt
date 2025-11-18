b5651deb5164df3f17acef7e2b1cf9ee10bfb35d
SkashEU <48493983+SkashEU@users.noreply.github.com>
2025-11-18 16:09:44 +0100

    fix: removed unused files from example project

--- examples/composeApp/src/jvmMain/kotlin/com/skash/forge/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.skash.forge
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
--- examples/composeApp/src/wasmJsMain/kotlin/com/skash/forge/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.skash.forge
-
-class WasmPlatform : Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
